2caed37c3f74fe2f5bf6ae0e5da41dfe36484de3
Alexandra Hertanu <alexandra.hertanu@yahoo.com>
2020-04-22 02:31:46 +0300

    Second commit

--- src/Printing.java
+++ src/Printing.java
@@ -1,16 +1,19 @@
 public class Printing {
     public void printNumbers(){
         int i;
-        for(i=1;i<=100;i++)
-            if(i%3==0&&i%5==0)
+        for(i=1;i<=100;i++) {
+            if (i % 7 == 0)
+                System.out.println("Rizz");
+            else if (i % 11 == 0)
+                System.out.println("Jazz");
+            else if (i % 3 == 0 && i % 5 == 0)
                 System.out.println("FizzBuzz");
-            else
-            if(i%3==0)
+            else if (i % 3 == 0)
                 System.out.println("Fizz");
-            else
-            if(i%5==0)
+            else if (i % 5 == 0)
                 System.out.println("Buzz");
             else
-                System.out.println(" "+i);
+                System.out.println(" " + i);
+        }
     }
 }
